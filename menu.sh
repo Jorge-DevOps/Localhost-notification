c68d94637f8fe9df26f36d30e741a826b2a35f7a
Jorge Rangel <jorgedevops20@gmail.com>
2021-11-20 09:19:22 -0500

    :construction: Firts Commit
    Commit

--- menu.sh
+++ menu.sh
@@ -7,34 +7,36 @@ menu="Welcome to LocalHost Notification\n\nPlease complete the information below
 
 # Show initial menu
 function showMenu() {
-	echo -e $menu
-	echo "1. Full Name"
-	read name
-	echo "2. Cellphone"
-	read phone
-	echo "Show IP directions"
-	show_ip_address()
+    linux_logo -L 9 -a
+    screenfetch
+    echo "LocalHost Notification"
+    echo "Version -> 1.0"
+    echo "Operationals systems, Section III"
+    echo "last release -> 29/11/2021"
+	
+    echo -e $menu
+    echo "1. Full Name"
+    read name
+    echo "2. Cellphone"
+    read phone
+    echo "Show IP directions"
+    show_ip_address
 }
 
 function validate_cellphone() {
-	# Validate if the Menu has 10 digits
-	if [[ $phone =~ ^[0-9]{10}$ ]]; then
-		echo "Cellphone is valid"
-	else
-		echo "Cellphone is invalid"
-		# TODO: Stop script
-	fi
+    # Validate if the Menu has 10 digits
+    if [[ $phone =~ ^[0-9]{10}$ ]]; then
+        echo "Cellphone is valid"
+    else
+        echo "Cellphone is invalid"
+        # TODO: Stop script
+    fi
 }
 
-function show_ip_address() {
-	# TODO: List for show ip address
-}
+# function show_ip_address() {
+# 	# TODO: List for show ip address
+# }
 
 ## Start application
-linux_logo -L 9 -a
-screenfetch
-echo "LocalHost Notification"
-echo "Version -> 1.0"
-echo "Operationals systems, Section III"
-echo "last release -> 29/11/2021"
-showMenu()
+
+showMenu
